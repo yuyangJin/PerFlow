a538f23da762cb35841dbf4053564bdb1fa59c33
yuyangJin <yuyuanjin47@gmail.com>
2021-11-21 16:18:39 +0800

    update filter and hotspot_detection pass

--- example/npb/cg.B.8/test.py
+++ example/npb/cg.B.8/test.py
@@ -4,6 +4,7 @@ proj_dir = os.environ['BAGUA_DIR']
 sys.path.append(proj_dir + r"/python")
 import json
 import perflow as pf
+from pag import * 
 
 pflow = pf.PerFlow()
 
@@ -11,11 +12,13 @@ pflow = pf.PerFlow()
 tdpag, ppag = pflow.run(binary = "./cg.B.8", cmd = "srun -n 8 ./cg.B.8", nprocs = 8)
 
 # Build a PerFlowGraph
-#V_comm = pflow.filter(pag.V, name = "MPI_*")
-#V_hot = pflow.hotspot_detection(V_comm)
+#V_comm = pflow.filter(tdpag.vs, type = PagType.MPI_NODE)
+#print(V_comm)
+V_hot = pflow.hotspot_detection(tdpag.vs)
+print([(v["name"], v['CYCAVGPERCENT']) for v in V_hot])
 #attrs = ["name", "comm-info", "debug-info", "time"] 
 #pflow.report(V_comm, V_hot, attrs)
 
 # 
-pflow.draw(tdpag, save_pdf = './cg.B.8.tdpag')
-pflow.draw(ppag, save_pdf = './cg.B.8.ppag')
+#pflow.draw(tdpag, save_pdf = './cg.B.8.tdpag')
+#pflow.draw(ppag, save_pdf = './cg.B.8.ppag')
--- python/pag.py
+++ python/pag.py
@@ -1,4 +1,17 @@
 from graph import *
+from enum import Enum
+
+class PagType(Enum):
+    BB_NODE = -12
+    INST_NODE = -11
+    ADDR_NODE = -10
+    CALL_IND_NODE = -7
+    CALL_REC_NODE = -6
+    CALL_NODE = -5
+    FUNC_NODE = -4
+    BRANCH_NODE = -2
+    LOOP_NODE = -1
+    MPI_NODE = 1
 
 
 class ProgramAbstractionGraph(BasicGraph):
