bc3116af0b32edda414df2c8f06ac102a8ccf44d
yuyangJin <yuyuanjin47@gmail.com>
2022-08-01 15:54:55 +0800

    adapt cluster jar

--- example/AE/pag_validation/pag_validation.py
+++ example/AE/pag_validation/pag_validation.py
@@ -9,7 +9,7 @@ from pag import *
 pflow = pf.PerFlow()
 
 # Run the binary and return a program abstraction graph
-tdpag, ppag = pflow.run(binary = "./cg.B.8", cmd = "srun -n 8 -p V132 ./cg.B.8", nprocs = 8)
+tdpag, ppag = pflow.run(binary = "./cg.B.x", cmd = "srun -n 8 ./cg.B.x", nprocs = 8)
 
 # draw pags and save as PDF files
 pflow.draw(tdpag, save_pdf = './cg.B.8.tdpag')
