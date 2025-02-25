e1fc407be614c0e69a62ac10a635acc427f6ee74
yuyangJin <yuyuanjin47@gmail.com>
2025-02-25 20:24:44 +0800

    [Feat] #13 Update the FlowNode class

--- perflow/flow/flow.py
+++ perflow/flow/flow.py
@@ -16,8 +16,30 @@ class FlowData:
 The sub-task node
 '''
 class FlowNode:
-    def __init__(self):
-        # To be implemented
+    def __init__(self, name, id, inputs):
+        self.m_name = name
+        self.m_id = id
+        self.m_inputs = inputs
+        self.m_outputs = None
+    
+    def __str__(self):
+        return f"FlowNode({self.m_name})"
+
+    def set_inputs(self, inputs):
+        self.m_inputs = inputs
+
+    def set_outputs(self, outputs):
+        self.m_outputs = outputs
+
+    def get_inputs(self):
+        return self.m_inputs
+
+    def get_outputs(self):
+        return self.m_outputs
+
+    # @abstractmethod
+    def run(self):
+        print('FlowNode runs virtially.')
         pass
 
 '''
